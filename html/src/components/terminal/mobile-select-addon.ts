090afb1d776955601f72fd21862255729fd9318b
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-11-16 12:28:38 -0500

    Test

--- html/src/components/terminal/mobile-select-addon.ts
+++ html/src/components/terminal/mobile-select-addon.ts
@@ -3,10 +3,12 @@ import { Terminal, ITerminalAddon } from 'xterm';
 export class MobileSelectAddon implements ITerminalAddon {
 
     private _terminal: Terminal;
+    private _core;
 
     activate(terminal: Terminal) {
         this._terminal = terminal;
         console.log('testing');
+        this._core = (this._terminal as any)._core;
     }
 
     public dispose() {}
