5bd5406aaad1d844e173b0e2761e3fcc03ff004f
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-11-16 14:15:13 -0500

    Update mobile-select-addon.ts

--- html/src/components/terminal/mobile-select-addon.ts
+++ html/src/components/terminal/mobile-select-addon.ts
@@ -8,59 +8,25 @@ export class MobileSelectAddon implements ITerminalAddon {
     activate(terminal: Terminal) {
         this._core = (terminal as any)._core;
         console.log('testing');
-        addEventListener('mousedown', ev => {
+        terminal.onData(ev => {
+            if (ev.match('x')) {
 
-            console.log('ev.x = ' + ev.x);
-            console.log('ev.y = ' + ev.y);
-            const coords = this._core._mouseService.getCoords(ev, terminal.element, terminal.cols, terminal.rows, false);
-            const x = coords[0] - 1;
-            const y = coords[1] - 1;
+                console.log('adding click copy callback');
 
-            this._core._selectionService._selectWordAt(coords, false);
-            //terminal.selectLines(y, y);
-            copyToClipboard(terminal.getSelection());
+                addEventListener('mousedown', ev => {
 
-            /*
-            //if (!navigator.userAgent.match(/ipad|ipod|iphone/i)) return;
-            let right = coords[0] - 1;
-            console.log('right = ' + right);
-            let left = coords[0] - 1;
-            console.log('left = ' + left);
-            const row = coords[1] - 1;
-            console.log('row = ' + row);
+                    console.log('ev.x = ' + ev.x);
+                    console.log('ev.y = ' + ev.y);
+                    const coords = this._core._mouseService.getCoords(ev, terminal.element, terminal.cols, terminal.rows, false);
+                    const x = coords[0] - 1;
+                    const y = coords[1] - 1;
 
-            terminal.select(right, row, 1);
-            console.log('char under click = ' + terminal.getSelection());
-
-            if (terminal.getSelection() === ' ') return;
-
-            console.log('not space');
-
-            while (terminal.getSelection() !== ' ' && right < terminal.cols) {
-                terminal.select(++right, row, 1);
+                    this._core._selectionService._selectWordAt(coords, false);
+                    copyToClipboard(terminal.getSelection());
+                });
             }
-
-            console.log('right after find = ' + right);
-
-            while (terminal.getSelection() !== ' ' && left > 0) {
-                terminal.select(--left, row, 1);
-                console.log('checking \'' + terminal.getSelection() + '\' for space');
-            }
-
-            console.log('left after find = ' + left);
-
-            return;
-
-            terminal.select(left, row, right - left + 1);
-
-            console.log('selected: ' + terminal.getSelection());
-
-            document.execCommand('copy');*/
         });
     }
-
-
-
     public dispose() {}
 }
 
