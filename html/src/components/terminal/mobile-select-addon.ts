9cf5d2600bbc02d727c0224dd4a7e085f6e4c52f
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-11-16 15:21:29 -0500

    Update mobile-select-addon.ts

--- html/src/components/terminal/mobile-select-addon.ts
+++ html/src/components/terminal/mobile-select-addon.ts
@@ -33,6 +33,7 @@ export class MobileSelectAddon implements ITerminalAddon {
             const coords = this._evToCoords(ev);
             this._core._selectionService._selectWordAt(coords, false);
             copyToClipboard(terminal.getSelection());
+            terminal.writeln('Text copied.');
             this._doSelect = false;
         });
     }
